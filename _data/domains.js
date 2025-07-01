97509dd623c53e1457b319a902a88d4948f24ab1
Elias Fretwell <eliasfretwell@protonmail.com>
2025-07-01 09:18:45 -0700

    Add page limit on local

--- _data/domains.js
+++ _data/domains.js
@@ -3,6 +3,8 @@ import * as fs from 'fs';
 export default function () {
 
   let scanData = JSON.parse(fs.readFileSync('./public/data/myscangov_homepage_audits.json','utf8'));
+  if (process.env.ELEVENTY_RUN_MODE === 'serve')
+    scanData = scanData.slice(0, 50);
 
   return scanData;
 }
