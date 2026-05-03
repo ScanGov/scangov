7030960e40b0cc09657a5599656bb6847a7f491c
Luke Fretwell <luke@lukefretwell.com>
2026-05-03 15:19:58 -0700

    Added light mode and CSS clean-up

--- _data/domains.js
+++ _data/domains.js
@@ -4,12 +4,13 @@ export default function () {
 
     let scanData = JSON.parse(readFileSync('./public/data/myscangov_homepage_audits.json', 'utf8'));
     if (process.env.ELEVENTY_RUN_MODE === 'serve') {
-        scanData = scanData.slice(0, 50)
-            .concat(
-                // Put a state and city domain in
-                scanData.find(d => d.urlkey === 'ca.gov'),
-                scanData.find(d => d.urlkey === 'cityofsacramento.gov')
-            );
+        let cutScanData = scanData.slice(0, 50);
+        // Put a state and city domain in
+        if (!cutScanData.find(d => d.urlkey === 'ca.gov'))
+            cutScanData.push(scanData.find(d => d.urlkey === 'ca.gov'));
+        if (!cutScanData.find(d => d.urlkey === 'cityofsacramento.gov'))
+            cutScanData.push(scanData.find(d => d.urlkey === 'cityofsacramento.gov'));
+        scanData = cutScanData;
     }
 
     return scanData;
