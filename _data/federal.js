d2537f11aa7e71727a776e0aa68d9ef9b845a292
Elias Fretwell <eliasfretwell@protonmail.com>
2026-07-15 13:24:56 -0700

    Add pagination to all rankings pages

--- _data/federal.js
+++ _data/federal.js
@@ -1,29 +1,27 @@
 import { default as domainData } from './domains.js';
-import { stateDomainList, cityDomainList, addRankingPosition } from './variables.js';
+import { stateDomainList, cityDomainList, countyDomainList, eduDomainList, addRankingPosition } from './variables.js';
 import * as fs from 'fs'
 
-export default function () {
-  let domainDataFilled = domainData()
+export default function() {
+    let domainDataFilled = domainData()
 
-  let federalDomainData = []
+    let federalDomainData = []
 
-  let noFedMap = new Map()
-  cityDomainList.forEach((l) => {
-    noFedMap.set(l, l)
-  })
-  stateDomainList.forEach((l) => {
-    noFedMap.set(l, l)
-  })
+    let noFedMap = new Map()
+    cityDomainList.forEach((l) => noFedMap.set(l, l));
+    stateDomainList.forEach((l) => noFedMap.set(l, l));
+    countyDomainList.forEach((l) => noFedMap.set(l, l));
+    eduDomainList.forEach((l) => noFedMap.set(l, l));
 
-  domainDataFilled.forEach((d) => {
-    if (!noFedMap.get(d.urlkey)) {
-      federalDomainData.push(d)
-    }
-  })
+    domainDataFilled.forEach((d) => {
+        if (!noFedMap.get(d.urlkey)) {
+            federalDomainData.push(d)
+        }
+    })
 
-  let federal = federalDomainData.sort(function (a, b) {
-    return parseInt(b.overallScore) - parseInt(a.overallScore)
-  })
+    let federal = federalDomainData.sort(function(a, b) {
+        return parseInt(b.overallScore) - parseInt(a.overallScore)
+    })
 
-  return addRankingPosition(federal, null);
+    return addRankingPosition(federal, null);
 }
